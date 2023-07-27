b44a659095744865e1e037f1a5f829cd52c418b2
u2knox <u2knox@gmail.com>
2023-07-27 13:32:53 +0600

    Update githubClient.ts

--- src/client/githubClient.ts
+++ src/client/githubClient.ts
@@ -6,6 +6,6 @@ export default new ApolloClient({
   cache,
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `Bearer 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`
+    Authorization: `Bearer github`+`_pat_11AKD4K7` + `I0rnAnIiLrF52f_zJJ7` + `YmuO2LDhEt7jjsNmpCKuB` + `wJIDddPbaXfIwD35` + `hY542S2A7KVjUUfD8H`
   }
 });
